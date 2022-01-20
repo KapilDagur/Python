f5d941c5abd47ee4f4c996bb464089519b0d401b
Kapil Dagur <95411822+Kapildagur@users.noreply.github.com>
2022-01-20 22:53:51 +0530

    Add files via upload

---
+++ 1). Introduction to Python/6).others.py
@@ -0,0 +1,14 @@
+#here we see printing type or storing in python
+# 1). Single court
+print('Hii')
+# 2). Double court
+print("Hey!!")
+# 3). Triple court
+print("""Hello
+how are you""")
+s1 = 'APPLE'
+s2 = "MANGO"
+s3 = """Banana
+Grapes
+etc"""
+print(s1,s2,s3)
---
+++ 1). Introduction to Python/7). Comment.py
@@ -0,0 +1,5 @@
+#Here we see comment
+#Comment is basically those world ya statment will not show in output but exist in code
+#Type of comment in python
+# 1).Single line comment, keyword defined in python for single line comment is " #statment " (Symbole name Hastage)
+# 2).Multi line comment, keyword defined in python for multil line comment is " """statment""" "(symbole name double court at three time)
